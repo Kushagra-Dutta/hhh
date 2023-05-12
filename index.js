1071820a2803273b53774a09c3101254ed507b0e
Kushagra Dutta <132273171+Kushagra-Dutta@users.noreply.github.com>
2023-05-13 00:00:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+
+
+var NumberOfButtons= document.querySelectorAll(".drum").length;
+
+for(var i=0;i<NumberOfButtons;i++){ 
+document.querySelectorAll(".drum")[i].addEventListener("click",function () {
+    var button=this.innerHTML;
+    MakeSound(button);
+    ButtonAnimation(button);
+});
+}
+
+document.addEventListener("keypress",function (event) {
+    MakeSound(event.key);
+    ButtonAnimation(event.key);
+});
+
+function MakeSound(key){
+    switch (key) {
+        case "w":
+            var audio = new Audio("./sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("./sounds/tom-2.mp3");
+            audio.play();
+            break;    
+        case "s":
+            var audio = new Audio("./sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("./sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("./sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        default:
+            break;
+    }
+};
+
+function ButtonAnimation(CurrentKey){
+    document.querySelector("."+ CurrentKey).classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector("."+ CurrentKey).classList.remove("pressed");
+    },100);
+}
+
